2591a5c0654dd84f3e816f80313a7f5d7e9dd0f6
karimy <karim.alrashdan14@gmail.com>
2023-02-05 13:13:41 +0100

    Create the error and loading messages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
 import "./App.css"
-import movieData from "./movieData";
-// import NavBar from "./NavBar"
+// import movieData from "./movieData";
 import DetailsPage from "./DetailsPage"
 import MovieContainer from "./MovieContainer";
+import { getData } from "./ApiCalls";
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      posters: movieData.movies,
+      posters: [],
+      // posters: movieData.movies,
+      isLoaded: false,
+      error: "",
       pickPoster: false,
       posterDetails: null,
     }
+  }
+  componentDidMount = () => {
+    getData()
+    .then((data) => {
+      this.setState({isLoaded:true, posters: data.movies})
+    })
+    .catch((error) => {
+      this.setState({isLoaded:true, error})
+      console.log("error", error)
+    })
+    console.log(this.state.posters)
   }
   showDetailsPage = (id) => {
     const selectMovie = this.state.posters.find(poster => poster.id === id)
@@ -22,20 +36,27 @@ class App extends Component {
     this.setState({pickPoster: false})
   }
   render() {
+    const { error, isLoaded } = this.state
     const isPosterPicked = this.state.pickPoster
-    if(isPosterPicked) {
-      return (
-        <main>
-          <DetailsPage posterDetails={this.state.posterDetails} showMain={this.showMainPage}/>
-        </main>
-      )
+    // this.componentDidMount()
+    if(error) {
+      return <div>You have an error: {error.message}</div>
+    } else if(!isLoaded) {
+      return <div>Loading...</div>
     } else {
-      return (
-        <main>
-          {/* <NavBar /> */}
-          <MovieContainer posters={this.state.posters} showDetails={this.showDetailsPage}/>
-        </main>
-      )
+      if(isPosterPicked) {
+        return (
+          <main>
+            <DetailsPage posterDetails={this.state.posterDetails} showMain={this.showMainPage}/>
+          </main>
+        )
+      } else {
+        return (
+          <main>
+            <MovieContainer posters={this.state.posters} showDetails={this.showDetailsPage}/>
+          </main>
+        )
+      }
     }
   }
 }
