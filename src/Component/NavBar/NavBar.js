8201818064da8d9a5ecfceb8272acb66bd6cd4b4
karimy <karim.alrashdan14@gmail.com>
2023-02-13 14:23:12 -0700

    Complete Route for NavBar

--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import "./NavBar.css"
-import { Link } from "react-router-dom"
+import { Route, Link } from "react-router-dom"
 
 class NavBar extends Component {
   constructor() {
@@ -10,38 +10,47 @@ class NavBar extends Component {
     }
   }
   render() {
-    if (this.props.mainpage) {
-      return (
-        <div className="navbar">
-          <h1>Rancid Tomatillos</h1>
-          <div className="searchSort">
-            <input className="search"
-              type="text"
-              placeholder="Search Movies"
-              name="search"
-              // value={this.state.search}
-            />
-            <div className="sortButton">
-              <button className="dropBtn">Sort
-                <i className="fa fa-caret-down"></i>
-              </button>
-              <div className="sortOptions">
-                <a href="#">Ascending ↑</a>
-                <a href="#">Descending ↓</a>
+    return(
+      <div>
+        <Route 
+          exact path="/" 
+          render= {() => 
+            <div className="navbar">
+              <h1>Rancid Tomatillos</h1>
+              <div className="searchSort">
+                <input className="search"
+                  type="text"
+                  placeholder="Search Movies"
+                  name="search"
+                  // value={this.state.search}
+                />
+                <div className="sortButton">
+                  <button className="dropBtn">Sort
+                    <i className="fa fa-caret-down"></i>
+                  </button>
+                  <div className="sortOptions">
+                    <a href="#">Ascending ↑</a>
+                    <a href="#">Descending ↓</a>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-        )
-    } else {
-      return (
-        <div className="navbar">
-          <h1>Rancid Tomatillos</h1>
-          <button onClick={() => {this.props.showMain()}}>
-            <Link to={"/"}>Return to Home</Link></button>
-        </div>
-      )
-    }
+          }
+        />                    
+        <Route 
+          exact path="/:id" 
+          render={() => 
+            <div className="navbar">
+              <h1>Rancid Tomatillos</h1>
+              <button onClick={() => {this.props.showMain()}}>
+                <Link to={"/"}>Return to Home</Link>
+              </button>
+            </div>  
+            }
+        />
+      </div>
+    )
   }
 }
+
 export default NavBar
